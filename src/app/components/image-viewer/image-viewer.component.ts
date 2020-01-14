6a7d574e67abcb608f0179acc1ae91d9f8d0098c
Sam Jones <samuelmorrisjones@gmail.com>
2020-01-14 17:55:07 +0000

    Step 4

--- src/app/components/image-viewer/image-viewer.component.ts
+++ src/app/components/image-viewer/image-viewer.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-image-viewer',
   templateUrl: './image-viewer.component.html',
   styleUrls: ['./image-viewer.component.css']
 })
-export class ImageViewerComponent implements OnInit {
+export class ImageViewerComponent {
+
+  @Input() open: boolean;
+  @Input() imageURL: string;
+
+  @Output() close = new EventEmitter();
 
   constructor() { }
-
-  ngOnInit() {
-  }
-
 }
